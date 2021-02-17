65cc67a546817c7742fbb7df5c93b99fd9ff7550
maxilukowski <maxi.lukowski@yahoo.com>
2021-02-17 13:05:58 +0100

    add basic table layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import axios from 'axios'
+import { useTable } from 'react-table'
 
 function App() {
   const [data, setData] = useState([])
   useEffect(() => {
     getData()
   }, [])
-  return <></>
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'First Name',
+        accessor: 'firstName',
+      },
+    ],
+    []
+  )
+  const tableInstance = useTable({ columns, data })
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = tableInstance
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+
   async function getData() {
     const { data } = await axios.get('http://localhost:3004/employees')
     setData(data)
